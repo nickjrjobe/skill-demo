8f424c9ee17706e6858b4480a326289a3ba8a8a2
nickjrjobe <103221521+nickjrjobe@users.noreply.github.com>
2022-05-01 16:58:38 -0700

    Update SkillDemo.java

--- SkillDemo.java
+++ SkillDemo.java
@@ -1,3 +1,6 @@
 public class SkillDemo{
-
+    public static boolean emailChecker(String email){
+        String domain = email.substring(email.length()-4,email.length());
+        return (domain == ".edu");
+    }
 }
